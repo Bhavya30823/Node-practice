a9a17a3986fa343e444f0c023e688b10b20b74ce
Bhavya Rastogi <bhavyarastogi08@gmail.com>
2026-02-26 01:44:49 +0530

    add sorting, filtering and pagination

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const userService = require("./services/userService");
 const taskService = require("./services/taskService");
 const authMiddleware = require("./middlewares/authMiddleware");
 
-app.use(express.json()); 
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
@@ -37,20 +37,33 @@ app.get("/profile", authMiddleware, (req, res) => {
 app.post("/create-task", authMiddleware, (req, res) => {
   try {
     const { title, status } = req.body; // object destructuring
-    const userEmail = req.user; 
+    const userEmail = req.user;
     const result = taskService.createTask(title, userEmail.email, status);
-    res.status(200).json({ message: "Task created successfully", task: result });
+    res
+      .status(200)
+      .json({ message: "Task created successfully", task: result });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
 });
 app.get("/get-tasks", authMiddleware, (req, res) => {
   try {
-    const userEmail = req.user; 
+    const userEmail = req.user;
+    const { status, search, sort, order } = req.query; // object destructuring
     const limit = parseInt(req.query.limit) || 10; // default limit is 10
     const offset = parseInt(req.query.offset) || 0; // default offset is 0
-    const result = taskService.getTasksForUser(userEmail.email, limit, offset);
-    res.status(200).json({ message: "Tasks retrieved successfully", tasks: result });
+    const result = taskService.getTasksForUser({
+      userEmail: userEmail.email,
+      limit: limit,
+      offset: offset,
+      status: status,
+      search: search,
+      sort: sort,
+      order: order,
+    });
+    res
+      .status(200)
+      .json({ message: "Tasks retrieved successfully", tasks: result });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
@@ -58,7 +71,7 @@ app.get("/get-tasks", authMiddleware, (req, res) => {
 app.delete("/delete-task/:id", authMiddleware, (req, res) => {
   try {
     const taskId = parseInt(req.params.id);
-    const userEmail = req.user; 
+    const userEmail = req.user;
     taskService.deleteTask(taskId, userEmail.email);
     res.status(200).json({ message: "Task deleted successfully" });
   } catch (error) {
@@ -68,10 +81,12 @@ app.delete("/delete-task/:id", authMiddleware, (req, res) => {
 app.put("/update-task/:id", authMiddleware, (req, res) => {
   try {
     const taskId = parseInt(req.params.id);
-    const userEmail = req.user; 
+    const userEmail = req.user;
     const { status } = req.body; // object destructuring
-    const result = taskService.updateTask(taskId, userEmail.email);
-    res.status(200).json({ message: "Task updated successfully", task: result });
+    const result = taskService.updateTask(taskId, userEmail.email, status);
+    res
+      .status(200)
+      .json({ message: "Task updated successfully", task: result });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
--- services/taskService.js
+++ services/taskService.js
@@ -1,13 +1,24 @@
 const taskRepository = require("../repositories/taskRepository");
 
+const allowedSortFields = ["userEmail", "title", "status"];
 function createTask(title, userEmail, status) {
     return taskRepository.createTask(title, userEmail, status);    
 }
 
-function getTasksForUser(userEmail, limit, offset) {
-    return taskRepository.getTasks()
-        .filter(task => task.userEmail === userEmail)
-        .slice(offset, offset + limit);
+function getTasksForUser({userEmail, limit, offset, status, search, sort, order}) {
+    let tasks = taskRepository.getTasks();
+    tasks = tasks.filter(task => task.userEmail === userEmail);
+    if (status) tasks = tasks.filter(task => task.status === status);
+    if (search) tasks = tasks.filter(task => task.title.includes(search));
+    // tasks = [...tasks].sort(...) to avoid mutating original array
+    if (sort && allowedSortFields.includes(sort)) tasks = [...tasks].sort((a, b) => {
+        if (order === "desc") {
+            return b[sort] > a[sort] ? 1 : -1;
+        } else {
+            return a[sort] > b[sort] ? 1 : -1;
+        }
+    });
+    return tasks.slice(offset, offset + limit);
 }
 
 function deleteTask(taskId, userEmail) {
